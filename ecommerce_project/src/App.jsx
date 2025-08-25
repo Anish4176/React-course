dd7a748c13724aaf272d2a794ca3ba7d5297d976
Anish <anishsingh41764176@gmail.com>
2025-08-26 00:30:14 +0530

    Added routes iand created Checkout page

--- ecommerce_project/src/App.jsx
+++ ecommerce_project/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Checkout from './components/Checkout'
 import Homepage from './components/Homepage'
-
+import { Routes,Route } from 'react-router'
 function App() {
   
 
   return (
     <>
-    <Homepage/>
+    <Routes>
+      <Route index element={<Homepage/>} ></Route>
+      <Route path='checkout' element={<Checkout/>} ></Route>
+    </Routes>
     </>
   )
 }
